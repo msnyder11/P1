bf5105a3c84a48721f042233dc3e3e3c655c791e
Max Snyder <maxsnyder@g.harvard.edu>
2014-09-16 16:36:47 -1000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# P1: Portfolio
+
+#### Project URL
+<http://helloworld.maxsnyder.me> 
+
+#### Project Description
+This project is a single page website that contains some information about me. It has my name, a picture, and links to my other projects. It also has my answers to some questions.
+
+#### Demo Information
+No demo for P1.
+
+#### Details The Instructor Or TA Need To Know
+Nothing.
+
+#### Plugins, Libraries, Packages, Or Outside Code Used
+Nothing.
---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Max Snyder</title>
+
+		<style>
+			h1{text-align:center;}
+			.image{text-align:center; border:1px solid black;}
+			.text{text-align:left; margin-left:20%; margin-top:90px;}
+		</style>
+	</head>
+	<body>
+		<h1>Max Snyder</h1>
+		
+		<div class = "image">
+			<img src="pic.jpg"; width = 30%; height = 30%/>
+		</div>
+		<div class="text"> 
+			<p>
+				<h2>
+					Project 2
+				</h2>
+				view: [link]  github: [link]
+
+				<h2>
+					Project 3
+				</h2>
+				view: [link]  github: [link]
+
+				<h2>
+					Project 4
+				</h2>
+				view: [link]  github: [link]
+
+			</p>			
+			
+			
+		</div>
+		<div class = "text">
+			<p>
+				<h2>
+					In 2+ sentences describe who you are and what you do
+				</h2>
+				I am Max Snyder and I make video games. I also watch tv, play video games, and soccer.
+
+				<h2>
+					Why are you taking this course?
+				</h2>
+				I am taking this course to learn more about making web applications.
+
+				<h2>
+					What previous experience do you have with programming?
+				</h2>
+				I got a bachelor's degree in software engineering at OIT and I also programmed some games.
+
+				<h2>
+					Are you working on a Mac, PC, or some other platform?
+				</h2>
+				PC.
+
+				<h2>
+					Anything else we should know?
+				</h2>
+				No.
+			</p>
+		</div>
+
+
+	</body>
+	
+</html>
